b04979dc8003e8870f6b14839d890204e2bc2a90
Hugo Courtial <hugo@courtial.me>
2016-07-10 15:38:16 +0200

    add argument support + rewrite

--- sniffealmidi.py
+++ sniffealmidi.py
@@ -14,69 +14,67 @@ Uses mido to send MIDI messages directly in python.
 
 v0.3
 By Sam Neurohack
-LICENCE : CC 
+LICENCE : CC
 '''
- 
+
+import argparse
 import mido
-from mido import Message
+import sys
 from time import sleep
-import types
-import random
 from scapy.all import *
 
-outport = mido.open_output('Arduino Leonardo')
-
-counter = 0
-
-def sendled(zzzport):
-	global counter
-	
-	zzz = zzzport % 127												# zzz = led color
-	msgout = mido.Message('note_on', note=counter, velocity=zzz)
-					
-	outport.send(msgout)
-	sleep(0.001)
-	counter += 1
-	if counter > 63:
-		counter = 0
-
-
 def print_summary(pkt):
+    global counter
+    out = 0
     if IP in pkt:
         ip_src=pkt[IP].src
         ip_dst=pkt[IP].dst
-        
-        
-    	if TCP in pkt:
-        	tcp_sport=pkt[TCP].sport
-        	tcp_dport=pkt[TCP].dport
 
-        	if tcp_sport < 50000:
-        		print " IP src " + str(ip_src) + " TCP sport " + str(tcp_sport) 
-        		sendled(tcp_sport)
-        	if tcp_dport < 50000:
-        		print " IP dst " + str(ip_dst) + " TCP dport " + str(tcp_dport)
-        		sendled(tcp_dport)
-        if UDP in pkt:
-        	udp_sport=pkt[UDP].sport
-        	udp_dport=pkt[UDP].dport
+    if TCP in pkt:
+        tcp_sport=pkt[TCP].sport
+        tcp_dport=pkt[TCP].dport
 
-        	if udp_sport < 50000:
-        		print " IP src " + str(ip_src) + " UDP sport " + str(udp_sport) 
-        		sendled(udp_sport)
-        	
-        	if udp_dport < 50000:
-        		print " IP dst " + str(ip_dst) + " UDP dport " + str(udp_dport)
-        		sendled(udp_dport)
+        if tcp_sport < 50000:
+            print " IP src " + str(ip_src) + " TCP sport " + str(tcp_sport)
+            out = tcp_sport
+        if tcp_dport < 50000:
+            print " IP dst " + str(ip_dst) + " TCP dport " + str(tcp_dport)
+            out = tcp_dport
 
+    if UDP in pkt:
+        udp_sport=pkt[UDP].sport
+        udp_dport=pkt[UDP].dport
 
-	if ARP in pkt and pkt[ARP].op in (1,2):
-		print " ARP"
-		sendled(67)
-        	
+        if udp_sport < 50000:
+            print " IP src " + str(ip_src) + " UDP sport " + str(udp_sport)
+            out = udp_sport
 
+        if udp_dport < 50000:
+            print " IP dst " + str(ip_dst) + " UDP dport " + str(udp_dport)
+            out = udp_dport
 
-def handle_error(self,request,client_address):		# All callbacks
-    pass
+    if ARP in pkt and pkt[ARP].op in (1,2):
+        print " ARP"
+        out = 67
 
-sniff(iface='en0', prn=print_summary, store=0)
+    outport.send(mido.Message('note_on', note=counter, velocity=out%127))
+    sleep(0.001)
+    counter += 1
+    if counter > 63:
+        counter = 0
+
+if __name__ == "__main__":
+    parse = argparse.ArgumentParser(description="Sniff traffic on a network interface and send it to BHOREAL MIDI controller")
+    parse.add_argument("-i", "--interface", help="network interface to be sniffed", default="en0")
+    parse.add_argument("-d", "--device", help="MIDI device", default="Arduino Leonardo")
+    args = parse.parse_args()
+
+    try:
+        outport = mido.open_output(args.device)
+    except IOError:
+        print "error: MIDI device not found"
+        sys.exit(1)
+
+    global counter
+    counter = 0
+    sniff(iface=args.interface, prn=print_summary, store=0)
